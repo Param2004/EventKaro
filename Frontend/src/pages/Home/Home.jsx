cb20465be5be39a79cef8ee9b714959878fd1173
Param2004 <kaushik.param2004@gmail.com>
2025-11-10 07:06:17 +0530

    deployment testing

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ import WhyChooseUs from '../../components/WhyChooseUs/WhyChooseUs'
 import Gallery from '../../components/Gallery/Gallery'
 import HowItWorks from '../../components/HowItWorks/HowItWorks'
 import Testimonials from '../../components/Testimonials/Testimonials'
-import StatsCounter from '../../components/StatsCounter/StatsCounter'
+// import StatsCounter from '../../components/StatsCounter/StatsCounter'
 import CTA from '../../components/CTA/CTA'
 import FAQ from '../../components/FAQ/FAQ'
 
@@ -27,7 +27,7 @@ const Home = () => {
         <Gallery/>
         <HowItWorks/>
         <Testimonials/>
-        <StatsCounter/>
+        {/* <StatsCounter/> */}
         <CTA/>
         <FAQ/>
         {/* <Header/> */}
